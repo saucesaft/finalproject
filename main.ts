29a12fbd6f0d2762c2dbe6d95bfff2f1e7d301e0
Eduardo Hernández Valdez <lalohdz.404@gmail.com>
2020-11-18 09:14:41 +0800

    target function

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 let start = 0
 let target_x = 0
 let target_y = 0
+let newx = 0
+let newy = 0
 input.onButtonPressed(Button.AB, function () {
     start = 1
 })
@@ -13,15 +15,39 @@ input.onGesture(Gesture.Shake, function () {
         # # # # #
         `)
     while (true) {
-        if (start != 0) {
-            basic.clearScreen()
-            for (let index = 0; index <= 2; index++) {
+        basic.clearScreen()
+        if (start == 1) {
+            for (let round = 0; round <= 2; round++) {
                 target_x = randint(0, 4)
                 target_y = randint(0, 4)
-                led.plot(target_x, target_y)
-                led.plot(target_x - 1, target_y - 1)
-                led.plot(target_x + 1, target_y + 1)
+                basic.clearScreen()
+                plotTarget(target_x, target_y)
+                basic.pause(2000)
             }
+            basic.showString("End")
+            break;
         }
     }
 })
+function plotTarget (x: number, y: number) {
+    led.plot(x, y)
+    newx = x
+    newy = y
+    for (let yfor = 0; yfor <= 2; yfor++) {
+        for (let xfor = 0; xfor <= 2; xfor++) {
+            if (xfor == 0) {
+                newy += -1
+            } else if (xfor == 2) {
+                newy += 1
+            }
+            if (yfor == 0) {
+                newx += -1
+            } else if (yfor == 2) {
+                newx += 1
+            }
+            led.plot(newx, newy)
+            newx = x
+            newy = y
+        }
+    }
+}
