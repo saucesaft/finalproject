13afbca6210cc2abd1efa6467195ad4d0c8b935c
Eduardo Hernández Valdez <lalohdz.404@gmail.com>
2020-11-19 13:45:38 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,8 @@ let target_x = 0
 let target_y = 0
 let newx = 0
 let newy = 0
+let rangingX = 0
+let rangingY = 0
 input.onButtonPressed(Button.AB, function () {
     start = 1
 })
@@ -20,9 +22,12 @@ input.onGesture(Gesture.Shake, function () {
             for (let round = 0; round <= 2; round++) {
                 target_x = randint(0, 4)
                 target_y = randint(0, 4)
+                basic.showString("Ready?")
                 basic.clearScreen()
                 plotTarget(target_x, target_y)
                 basic.pause(2000)
+                basic.clearScreen()
+                gameStart(target_x, target_y)
             }
             basic.showString("End")
             break;
@@ -48,6 +53,28 @@ function plotTarget (x: number, y: number) {
             led.plot(newx, newy)
             newx = x
             newy = y
+        }
+    }
+}
+function gameStart (currentX: number, currentY: number) {
+    while (true) {
+        while (input.buttonIsPressed(Button.B)) {
+            basic.clearScreen()
+            led.plot(rangingX, rangingY)
+            rangingX += 1
+            if (rangingX > 4) {
+                rangingX = 0
+            }
+            basic.pause(200)
+        }
+        while (input.buttonIsPressed(Button.A)) {
+            basic.clearScreen()
+            led.plot(rangingX, rangingY)
+            rangingY += 1
+            if (rangingY > 4) {
+                rangingY = 0
+            }
+            basic.pause(200)
         }
     }
 }
