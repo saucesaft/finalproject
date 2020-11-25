71f5fbcaf7d691af64c35ad2b06ab7cbc0be6791
Eduardo Hernández Valdez <lalohdz.404@gmail.com>
2020-11-25 08:33:49 +0800

    shooting almost done

--- main.py
+++ main.py
@@ -1,10 +1,89 @@
-def on_forever():
-    if input.is_gesture(Gesture.SHAKE):
-        basic.show_leds("""
-            # # # . .
-            # . . # #
-            . # # # .
-            . . # . #
-            # # # # #
-            """)
-basic.forever(on_forever)
+rangingX = 0
+alreadyStarted = 0
+start = 0
+rangingY = 0
+intro = 0
+target_x = 0
+target_y = 0
+newx = 0
+newy = 0
+
+def on_button_pressed_a():
+    global rangingX, alreadyStarted
+    rangingX += 1
+    if rangingX == 5:
+        rangingX = 0
+    basic.pause(200)
+    alreadyStarted = 1
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    global start
+    start = 1
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    global rangingY, alreadyStarted
+    rangingY += 1
+    if rangingY == 5:
+        rangingY = 0
+    basic.pause(200)
+    alreadyStarted = 1
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_gesture_shake():
+    global intro, target_x, target_y
+    intro = 1
+    basic.show_leds("""
+        # # # . .
+        # . . # #
+        # # # # .
+        . . # . #
+        # # # # #
+        """)
+    while True:
+        basic.clear_screen()
+        if start == 1:
+            for round2 in range(3):
+                target_x = randint(0, 4)
+                target_y = randint(0, 4)
+                basic.show_string("Ready?")
+                basic.clear_screen()
+                plotTarget(target_x, target_y)
+                basic.pause(2000)
+                basic.clear_screen()
+                gameStart(target_x, target_y)
+            basic.show_string("End")
+            break
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def plotTarget(x: number, y: number):
+    global newx, newy
+    led.plot(x, y)
+    newx = x
+    newy = y
+    for yfor in range(3):
+        for xfor in range(3):
+            if xfor == 0:
+                newy += -1
+            elif xfor == 2:
+                newy += 1
+            if yfor == 0:
+                newx += -1
+            elif yfor == 2:
+                newx += 1
+            led.plot(newx, newy)
+            newx = x
+            newy = y
+def gameStart(currentX: number, currentY: number):
+    while True:
+        if alreadyStarted == 0:
+            led.plot(0, 0)
+        else:
+            pass
+        led.plot(rangingX, rangingY)
+        if input.button_is_pressed(Button.AB) or input.acceleration(Dimension.Z) > 1000:
+            basic.clear_screen()
+            basic.show_number(rangingX)
+            basic.show_number(rangingY)
+            break
--- main.ts
+++ main.ts
@@ -1,14 +1,19 @@
 let start = 0
+let intro = 0
+let dart: game.LedSprite = null
 let target_x = 0
 let target_y = 0
 let newx = 0
 let newy = 0
-let rangingX = 0
-let rangingY = 0
+let shouldLoopNext = 0
 input.onButtonPressed(Button.AB, function () {
     start = 1
 })
+input.onButtonPressed(Button.B, function () {
+	
+})
 input.onGesture(Gesture.Shake, function () {
+    intro = 1
     basic.showLeds(`
         # # # . .
         # . . # #
@@ -16,6 +21,7 @@ input.onGesture(Gesture.Shake, function () {
         . . # . #
         # # # # #
         `)
+    dart = game.createSprite(0, 0)
     while (true) {
         basic.clearScreen()
         if (start == 1) {
@@ -58,23 +64,30 @@ function plotTarget (x: number, y: number) {
 }
 function gameStart (currentX: number, currentY: number) {
     while (true) {
-        while (input.buttonIsPressed(Button.B)) {
-            basic.clearScreen()
-            led.plot(rangingX, rangingY)
-            rangingX += 1
-            if (rangingX > 4) {
-                rangingX = 0
+        if (input.buttonIsPressed(Button.B) && !(input.buttonIsPressed(Button.A))) {
+            dart.change(LedSpriteProperty.X, 1)
+            if (shouldLoopNext == 1) {
+                dart.set(LedSpriteProperty.X, 0)
+                shouldLoopNext = 0
+            }
+            if (dart.get(LedSpriteProperty.X) == 4) {
+                shouldLoopNext = 1
+            }
+            basic.pause(200)
+        } else if (input.buttonIsPressed(Button.A) && !(input.buttonIsPressed(Button.B))) {
+            dart.change(LedSpriteProperty.Y, 1)
+            if (shouldLoopNext == 1) {
+                dart.set(LedSpriteProperty.Y, 0)
+                shouldLoopNext = 0
+            }
+            if (dart.get(LedSpriteProperty.Y) == 4) {
+                shouldLoopNext = 1
             }
             basic.pause(200)
         }
-        while (input.buttonIsPressed(Button.A)) {
+        if (input.buttonIsPressed(Button.AB) || input.acceleration(Dimension.Z) > 1000) {
             basic.clearScreen()
-            led.plot(rangingX, rangingY)
-            rangingY += 1
-            if (rangingY > 4) {
-                rangingY = 0
-            }
-            basic.pause(200)
+            break;
         }
     }
 }
